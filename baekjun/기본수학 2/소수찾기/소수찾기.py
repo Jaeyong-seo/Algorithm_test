1cafce3d2364c8a1d720c740630820a80e46a059
seojaeyong <wg7363@naver.com>
2021-07-21 11:24:19 +0900

    2021-07-21 스터디 - 소수찾기 풀이완료!

---
+++ baekjun/기본수학 2/소수찾기/소수찾기.py
@@ -0,0 +1,14 @@
+def PrimeNumber_Cnt(array):
+    cnt = 0
+    for num in array:
+        divisor_list = []
+        for n in range(1,num+1):
+            if num % n == 0:
+                divisor_list.append(n)
+        if len(divisor_list) == 2:
+           cnt += 1
+    return cnt
+
+times = int(input())
+num_list = list(map(int,input().split()))
+print( PrimeNumber_Cnt( num_list ))

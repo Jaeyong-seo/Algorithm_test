dcb5e885d51a4f6eb366574dbdd393026574d26e
seojaeyong <wg7363@naver.com>
2021-07-23 17:13:31 +0900

    프로그래머스 다트게임

--- programmers/레벨1/다트 게임/다트 게임.py
+++ programmers/레벨1/다트 게임/다트 게임.py
@@ -1,9 +1,28 @@
 def solution(dartResult):
-    answer = list(dartResult.replace('*','* ').replace('#','# ').replace('T','T ').replace('D','D ').replace('S','S ').split())
+    score = [0]
+    # ['1S', '#', '2D', '*', '3T'] 문자열 변환
+    dartgame = list(dartResult.replace('*','* ').replace('#','# ').replace('T','T ').replace('D','D ').replace('S','S ').split())
 
-    for dart in answer:
+    for dart in dartgame:
+        # print(score)
+        #점수다트
+        if len(dart) > 1:
+            point, area = int(dart[:-1]),dart[-1]
+            if area == 'S':
+                score.append(point)
+            elif area == 'D':
+                score.append(point ** 2)
+            elif area == 'T':
+                score.append(point ** 3)
+        #스타상, 아차상
+        else:
+            if dart == '*':
+                score[-1] = score[-1] * 2
+                if len(score) > 1:
+                    score[-2] = score[-2] * 2
+            elif dart == '#':
+                score[-1] = -(score[-1])
 
+    return sum(score)
 
-    return answer
-
-print(solution('1S#2D*3T'))
+print(solution(' 1S**2T*3S '))

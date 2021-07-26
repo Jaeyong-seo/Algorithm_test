87523a15ed632c036c5bc84ad0fcb23e98fd56b2
seojaeyong <wg7363@naver.com>
2021-07-26 09:35:56 +0900

    알고리즘수업 SORT 구현

--- 알고리즘 수업/정렬(Sort)/simple_sort.py
+++ 알고리즘 수업/정렬(Sort)/simple_sort.py
@@ -1,32 +1,39 @@
-def insertion_sort(arr):
-    num = 0
-    for idx in range(len(arr)-1):
-        if arr[idx] > arr[idx+1]:
-            num = arr[idx]
-            arr[idx] = arr[idx+1]
-            arr[idx + 1] = num
+def insertion_sort(arr_copy):
+    arr = arr_copy.copy()
+    for check in range(len(arr)):
+        for ck in range(check,0,-1):
+            if arr[ck] < arr[ck-1]:
+                arr[ck],arr[ck-1] = arr[ck-1],arr[ck]
     return arr
 
-def selection_sort():
+def selection_sort(arr_copy):
+    arr = arr_copy.copy()
+    for check in range(len(arr)-1):
+        m_val = arr.index(min(arr[check+1:]))
+        if arr[check] > arr[m_val]:
+            arr[check], arr[m_val] = arr[m_val], arr[check]
+    return arr
 
-    return []
+def bubble_sort(arr_copy):
+    arr = arr_copy.copy()
+    for check in range(len(arr)-1,0,-1):
+        for ck in range(check):
+            if arr[ck] > arr[ck+1]:
+                arr[ck],arr[ck+1] = arr[ck+1],arr[ck]
+    return arr
 
-def bubble_sort():
+# n = int(input())
+num_list = [5,4,3,2,1]
 
-    return []
-
-n = int(input())
-num_list = []
-
-for _ in range(n):
-    num = int(input())
-    num_list.append(num)
+# for _ in range(n):
+#     num = int(input())
+#     num_list.append(num)
 
 insertion_sorted_list = insertion_sort( num_list )
 print(" ".join(map(str, insertion_sorted_list)))
 
-# selection_sorted_list = selection_sort()
-# print(" ".join(map(str, selection_sorted_list)))
+selection_sorted_list = selection_sort(num_list)
+print(" ".join(map(str, selection_sorted_list)))
 
-# bubble_sorted_list = bubble_sort()
-# print(" ".join(map(str, bubble_sorted_list)))
+bubble_sorted_list = bubble_sort(num_list)
+print(" ".join(map(str, bubble_sorted_list)))

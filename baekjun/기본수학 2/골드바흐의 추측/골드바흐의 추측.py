ad4f0a8aafab43388bc215be23c4338c573864ce
RunLearn <wg7363@naver.com>
2021-07-29 10:52:49 +0900

    알고리즘수업 _ 원형 큐

--- baekjun/기본수학 2/골드바흐의 추측/골드바흐의 추측.py
+++ baekjun/기본수학 2/골드바흐의 추측/골드바흐의 추측.py
@@ -1,34 +1,28 @@
-import math
+import sys
 
 def solution(N):
     check = [True] * (N+1)
     Prime = []
-
+    
     # N까지 소수목록 생성
-    for i in range(2, N+1):
-        for j in range(i+i,N+1,i):
+    for i in range(2, N):
+        for j in range(i+i,N,i):
             if check[i]:
                 check[j] = False
 
-    for i in range(2,N+1):
+    for i in range(2,N):
         if check[i]:
             Prime.append(i)
-    
     #골드바흐
-    point = math.ceil((len(Prime)-1) ** 0.5)
+    for i in range(len(Prime)-1,-1,-1):
+        for j in range(len(Prime)-1,-1,-1):
+            if Prime[i] == N - Prime[j] and i < j: 
+                return [Prime[i], Prime[j]]
 
-    for i in range(point,-1,-1):
-        for j in range(len(Prime)-1,0,-1):
-            if Prime[j] + Prime[i] == N:
-                if i < j:
-                    return [Prime[i], Prime[j]]
-                else:
-                    return [Prime[j], Prime[i]]
 
-import sys
 
 times = int(sys.stdin.readline())
 for _ in range(times):
     N = int(sys.stdin.readline())
-    print(' '.join(map(str,solution(N))))
-    # print(f'{min(solution(N))} {max(solution(N))}')
+    answer = solution(N)
+    print(f'{answer[0]} {answer[1]}')
--- 알고리즘 수업/엔큐(enqueue)/enque.py
+++ 알고리즘 수업/엔큐(enqueue)/enque.py
@@ -1,31 +1,46 @@
-# [..., 1,2,3,4,5,6,7, ...]
 
 class Circular_Queue:
-    def __init__(self, n):
-        self.array = [None] * n
-        # self.array = [None for _ in range(n)]
-        self.front = 0 % n
-        self.back = 0 % n
+    def __init__(self,n):
+        self.array = [None] * Q_size
+        self.front = 0 % Q_size
+        self.back = 0 % Q_size
 
 
     def enQueue(self, num):
         self.array[self.back] = int(num)
+        self.back = (self.back + 1) % Q_size
         print(self.array)
-        self.back += 1
+        print(self.back)
+        
     
     def deQueue(self):
-        self.array[self.back] = None
+        last_val = self.array[self.front]
+        self.array[self.front] = None
         print(self.array)
-        self.back -= 1
-        pass
+        self.front = (self.front + 1) % Q_size
+        return last_val
         
     def size(self):
-        pass
+        return abs(self.front - self.back)
     
     def empty(self):
-        pass
+        if self.array:
+            return 0
+        if self.front == self.back:
+            return 1
+    
+    def front_val(self):
+        if self.empty == 1:
+            return -1
+        return self.array[self.front]
+    
+    def back_val(self):
+        if self.empty == 1:
+            return -1
+        return self.array[self.back-1]
 
     
+
 def run_cmd_with_queue(Cqueue_obj, command):
     cmd_type = command[0]
     
@@ -38,12 +53,15 @@ def run_cmd_with_queue(Cqueue_obj, command):
         print(Cqueue_obj.size())
     elif cmd_type == "empty":
         print(Cqueue_obj.empty())
+    elif cmd_type == "front":
+        print(Cqueue_obj.front_val())
+    elif cmd_type == "back":
+        print(Cqueue_obj.back_val())
+
+Q_size = 6
 
 n = int(input())
 Cqueue_obj = Circular_Queue(n)
 
 for _ in range(n):
     run_cmd_with_queue(Cqueue_obj, input().split())
-
-
-

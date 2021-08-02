458a74e56498076d121a27291b015ded29fd2d3a
RunLearn <wg7363@naver.com>
2021-08-02 10:55:32 +0900

    알고리즘 수업, 골드바흐 수정

--- baekjun/기본수학 2/골드바흐의 추측/골드바흐의 추측.py
+++ baekjun/기본수학 2/골드바흐의 추측/골드바흐의 추측.py
@@ -1,9 +1,9 @@
 import sys
+import math
 
 def solution(N):
-    check = [True] * (N+1)
+    check = [True] * (N)
     Prime = []
-    
     # N까지 소수목록 생성
     for i in range(2, N):
         for j in range(i+i,N,i):
@@ -14,12 +14,10 @@ def solution(N):
         if check[i]:
             Prime.append(i)
     #골드바흐
-    for i in range(len(Prime)-1,-1,-1):
-        for j in range(len(Prime)-1,-1,-1):
+    for i in range(math.ceil(len(Prime)*0.5),-1,-1):
+        for j in range(len(Prime)-1,i,-1):
             if Prime[i] == N - Prime[j] and i < j: 
-                return [Prime[i], Prime[j]]
-
-
+                return Prime[i], Prime[j]
 
 times = int(sys.stdin.readline())
 for _ in range(times):
---
+++ 알고리즘 수업/큐(queue)/큐추가문제2.py
@@ -0,0 +1,6 @@
+N, M = map(int,input().split())
+Card_list = list(map(int,input().split()))
+
+Card_dummy = list(range(1,N+1))
+
+print(Card_dummy)

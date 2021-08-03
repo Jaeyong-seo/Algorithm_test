faf7a3e20f22fbf1e52c7561b4d305a70e847b04
RunLearn <wg7363@naver.com>
2021-08-03 18:02:18 +0900

    백준 골드바흐 풀이성공

--- baekjun/기본수학 2/골드바흐의 추측/골드바흐_v2.py
+++ baekjun/기본수학 2/골드바흐의 추측/골드바흐_v2.py
@@ -1,25 +1,28 @@
 import sys
-import math
 
-def solution(N):
+def Prime_num(N):
     check = [True] * (N)
-
     # N까지 소수목록 생성
     for i in range(2, int(N**0.5)+1):
         for j in range(i+i,N,i):
             if check[i]:
                 check[j] = False
 
-    Prime = [i for i in range(2,N) if check[i]]
+    return [i for i in range(2,N) if check[i]]
 
+def solution(N):
+    Prime = Prime_num(N)
     #골드바흐
-    for i in range(int(len(Prime)*0.5)+1,-1,-1):
-        for j in range(len(Prime)-1,i,-1):
-            if Prime[i] == N - Prime[j] and i < j: 
-                return Prime[i], Prime[j]
+    idx = max([i for i in range(len(Prime)) if Prime[i] <= N/2])
+
+    for i in range(idx,-1,-1):
+        for j in range(i,len(Prime)):
+            if Prime[i] + Prime[j] == N:
+                return [Prime[i],Prime[j]]
 
 times = int(sys.stdin.readline())
 for _ in range(times):
     N = int(sys.stdin.readline())
     answer = solution(N)
     print(f'{answer[0]} {answer[1]}')
+

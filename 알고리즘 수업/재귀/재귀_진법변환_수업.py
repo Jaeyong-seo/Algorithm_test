15731ddb1e3f8b0f3580ad81d81ba9594c00f5b3
RunLearn <wg7363@naver.com>
2021-08-12 11:01:41 +0900

    알고리즘 수업

---
+++ 알고리즘 수업/재귀/재귀_진법변환_수업.py
@@ -0,0 +1,3 @@
+num_str, b = input().split()
+decimal_num = int(num_str, int(b))
+print(decimal_num)
--- 알고리즘 수업/큐(queue)/큐추가문제2.py
+++ 알고리즘 수업/큐(queue)/큐추가문제2.py
@@ -1,12 +1,28 @@
 from collections import deque
 
-N, M = map(int,input().split())
-pop_list = list(map(int,input().split()))
+n, m = map(int, input().split())
+target_list = map(int, input().split())
 
-Card_dummy = deque(range(1,N+1))
+min_move = 0
+num_list = deque([i for i in range(1, n+1)])
 
-for find_idx in pop_list:
-    num_idx = Card_dummy.index(find_idx)
+for find_idx in target_list:
+    num_idx = num_list.index(find_idx)
     move_cnt = num_idx
 
-    if move_cnt > ()
+    if move_cnt > (len(num_list) - move_cnt):
+        move_cnt -= len(num_list)
+    
+    # print(move_cnt)
+
+    min_move += abs(move_cnt)
+
+    # num_list.append(num_list.popleft())
+    # num_list.appendleft(num_list.pop())
+
+    num_list.rotate(-move_cnt)
+    num_list.popleft()
+
+    # print(num_list)
+
+print(min_move)

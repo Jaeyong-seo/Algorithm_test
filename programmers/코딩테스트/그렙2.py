877fa28ab34d20cd9772317878df3002e99c5378
RunLearn <wg7363@naver.com>
2021-08-23 09:51:33 +0900

    그렙 코테 리뷰

--- programmers/코딩테스트/그렙2.py
+++ programmers/코딩테스트/그렙2.py
@@ -1,42 +1,35 @@
+# Luhn공식 구현
+def Luhn(N):
+    case_1 = 0
+    case_2 = 0
 
+    for idx,val in enumerate(N):
+        val = int(val)
+
+        if idx % 2 == 0:
+            val = val*2
+            if val < 10:
+                case_1 += val
+            else:
+                val = int(str(val)[0]) + int(str(val)[1])
+                case_1 += val
+        else:
+            case_2 += val
+
+    if (case_1 + case_2) % 10 == 0:
+        return 1
+    else:
+        return 0
 
 def solution(card_numbers):
     #신용카드 번호 전처리
     numbers = [i.replace('-','',4) for i in card_numbers]
-
-    #Luhn공식 구현
     answer = []
-    for number in numbers:
-        for idx, num in enumerate(number):
-            arr1 = []
-            arr2 = []
-
-            if idx % 2 == 0:
-                val = int(num)*2
-                if val < 10:
-                    arr1.append(val)
-                else:
-                    arr1.append(str(val)[0]+str(val)[1])
-            else:
-                arr2.append(val)
-
-            try:
-                if sum(arr1) % sum(arr2)==0:
-                    answer.append(1)
-                else:
-                    answer.append(0)
-            except:
-                pass
-    return answer
-
-            
-
-
-
     
+    for number in numbers:
+        answer.append(Luhn(number))
 
-    return numbers
-
+    return answer
 
 arr = ["3285-3764-9934-2453", "3285376499342453", "3285-3764-99342453", "328537649934245", "3285376499342459", "3285-3764-9934-2452"]
 print(solution(arr))
